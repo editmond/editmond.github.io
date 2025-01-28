a7663ad76cf9071c4672f55f6d7ce0f3f9ff451f
editmond <42686022+editmond@users.noreply.github.com>
2025-01-28 17:22:20 +1100

    Time based greetings

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,24 @@
 const myHeading = document.querySelector("h1");
 
-let isDay = true
-document.querySelector("h1").addEventListener("click", function () {
-  // alert("Ouch! Stop poking me!");
-  isDay = !(isDay);
-  myHeading.textContent = isDay ? "G'day" : "Good Evening";
-});
+let isDay = true;
+let greeting = "G'day";
+let now = new Date();
+let hour = now.getHour();
+if (hour < 5){
+  greeting = "go to sleep!";
+}
+if (15 < hour && hour < 18){
+  greeting = "Good afternoon!";
+}
+if (18 < hour){
+  greeting = "Good evening!";
+}
+myHeading.textContent = greeting;
+// document.querySelector("h1").addEventListener("click", function () {
+//   // alert("Ouch! Stop poking me!");
+//   isDay = !(isDay);
+//   myHeading.textContent = isDay ? "G'day" : "Good Evening";
+// });
 
 const pfpImage = document.querySelector("img");
 pfpImage.addEventListener("click", () => {
