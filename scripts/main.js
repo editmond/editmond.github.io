063feac707b0f98be84ad9c55d2c53a303babd4a
editmond <edmondcheng@gmail.com>
2026-02-12 19:57:57 +1100

    Edit main script to work with all pages.

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,22 @@
 // Time based greetings
-const myHeading = document.querySelector("h1");
-let greeting = "G'day!";
-let now = new Date();
-let hours = now.getHours();
-// let minutes = now.getMinutes();
-// let seconds = now.getSeconds();
-if (hours < 5){
-  greeting = "Go to sleep!";
+const myHeading = document.getElementById("maintitle");
+if (myHeading) {
+  let greeting = "G'day!";
+  let now = new Date();
+  let hours = now.getHours();
+  // let minutes = now.getMinutes();
+  // let seconds = now.getSeconds();
+  if (hours < 5){
+    greeting = "Go to sleep!";
+  }
+  if (15 <= hours && hours < 18){
+    greeting = "Good afternoon!";
+  }
+  if (18 <= hours){
+    greeting = "Good evening!";
+  }
+  myHeading.textContent = greeting;
 }
-if (15 <= hours && hours < 18){
-  greeting = "Good afternoon!";
-}
-if (18 <= hours){
-  greeting = "Good evening!";
-}
-myHeading.textContent = greeting;
-
 // click image easter egg
 const pfpImage = document.querySelector("img");
 pfpImage.addEventListener("click", () => {
